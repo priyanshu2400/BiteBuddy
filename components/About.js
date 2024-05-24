a96681912fa95c8b5dd3ccb94a52af71e59f4332
priyanshu2400 <priyanshujha024@gmail.com>
2024-05-24 13:13:50 +0530

    bug fix

--- components/About.js
+++ components/About.js
@@ -4,7 +4,7 @@ const About = () => {
         <div className=" m-10 p-10 flex flex-col justify-center items-center">
             <h1 className="font-bold pt-10">GITHUB PROFILE</h1>
             {/* <User name="Priyanshu" contact="priyanshu@gmail.com" address="bengaluru"/> */}
-            <UserClass name="Samiksha" contact="Samiksha@gmail.com" address="bengaluru"/>
+            <UserClass name="Priyanshu" contact="Priyanshu@gmail.com" address="bengaluru"/>
         </div>
     )
 }
